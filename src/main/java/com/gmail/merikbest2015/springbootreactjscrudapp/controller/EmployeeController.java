69ef1a56f10ca3fe4b4e96ad42c56a3e98215ff0
merik <merikbest2015@gmail.com>
2020-12-07 18:42:49 +0300

    Added ControllerUtils. Fixed EmployeeController and Employee entity.

--- src/main/java/com/gmail/merikbest2015/springbootreactjscrudapp/controller/EmployeeController.java
+++ src/main/java/com/gmail/merikbest2015/springbootreactjscrudapp/controller/EmployeeController.java
@@ -4,9 +4,12 @@ import com.gmail.merikbest2015.springbootreactjscrudapp.exception.ResourceNotFou
 import com.gmail.merikbest2015.springbootreactjscrudapp.model.Employee;
 import com.gmail.merikbest2015.springbootreactjscrudapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,37 +27,57 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        return employeeService.findAll();
+    public ResponseEntity<?> getAllEmployees() {
+        List<Employee> employees = employeeService.findAll();
+
+        return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @PostMapping("/employees")
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeService.save(employee);
+    public ResponseEntity<?> createEmployee(@RequestBody @Valid Employee employee, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            Employee savedEmployee = employeeService.save(employee);
+
+            return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+        }
     }
 
     @GetMapping("/employees/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+    public ResponseEntity<?> getEmployeeById(@PathVariable Long id) {
         Employee employee = employeeService.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
 
-        return ResponseEntity.ok(employee);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PutMapping("/employees/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
-        Employee employee = employeeService.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+    public ResponseEntity<?> updateEmployee(
+            @PathVariable Long id,
+            @RequestBody @Valid Employee employeeDetails,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
 
-        employee.setFirstName(employeeDetails.getFirstName());
-        employee.setLastName(employeeDetails.getLastName());
-        employee.setCity(employeeDetails.getCity());
-        employee.setAddress(employeeDetails.getAddress());
-        employee.setTelephone(employeeDetails.getTelephone());
+            return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+        } else {
+            Employee employee = employeeService.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
 
-        Employee updatedEmployee = employeeService.save(employee);
+            employee.setFirstName(employeeDetails.getFirstName());
+            employee.setLastName(employeeDetails.getLastName());
+            employee.setCity(employeeDetails.getCity());
+            employee.setAddress(employeeDetails.getAddress());
+            employee.setTelephone(employeeDetails.getTelephone());
 
-        return ResponseEntity.ok(updatedEmployee);
+            employeeService.save(employee);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 
     @DeleteMapping("/employees/{id}")
@@ -66,6 +89,7 @@ public class EmployeeController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
 
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+//        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/gmail/merikbest2015/springbootreactjscrudapp/model/Employee.java
+++ src/main/java/com/gmail/merikbest2015/springbootreactjscrudapp/model/Employee.java
@@ -2,33 +2,32 @@ package com.gmail.merikbest2015.springbootreactjscrudapp.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "employees")
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NonNull
+    @NotBlank(message = "Empty field!")
     @Column(name = "first_name")
     private String firstName;
-    @NonNull
+    @NotBlank(message = "Empty field!")
     @Column(name = "last_name")
     private String lastName;
-    @NonNull
+    @NotBlank(message = "Empty field!")
     @Column(name = "city")
     private String city;
-    @NonNull
+    @NotBlank(message = "Empty field!")
     @Column(name = "address")
     private String address;
-    @NonNull
+    @NotBlank(message = "Empty field!")
     @Column(name = "telephone")
     private String telephone;
 }
